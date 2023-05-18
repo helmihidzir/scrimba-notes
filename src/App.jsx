e44de24585b173c5358a45812e62fc4edbc6f692
helmihidzir <helmihidzir@gmail.com>
2023-05-18 11:05:51 +0800

    Add debouncing

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { notesCollection, db } from "./firebase.js";
 export default function App() {
     const [notes, setNotes] = React.useState([])
     const [currentNoteId, setCurrentNoteId] = React.useState("")
+    const [tempNoteText, setTempNoteText] = React.useState("")
 
     const currentNote =
         notes.find(note => note.id === currentNoteId)
@@ -26,7 +27,7 @@ export default function App() {
             setNotes(notesArr)
         })
 
-        return () => unsubscribe
+        return unsubscribe
     }, [])
 
     React.useEffect(() => {
@@ -34,6 +35,21 @@ export default function App() {
             setCurrentNoteId(notes[0]?.id)
         }
     }, [notes])
+
+    React.useEffect(() => {
+        if(currentNote) {
+            setTempNoteText(currentNote.body)
+        }
+    }, [currentNote])
+
+    React.useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if(tempNoteText !== currentNote.body) {
+                updateNote(tempNoteText)
+            }
+        }, 500)
+        return () => clearTimeout(timeoutId)
+    }, [tempNoteText])
 
     async function createNewNote() {
         const newNote = {
@@ -79,8 +95,8 @@ export default function App() {
                             deleteNote={deleteNote}
                         />
                         <Editor
-                            currentNote={currentNote}
-                            updateNote={updateNote}
+                            tempNoteText={tempNoteText}
+                            setTempNoteText={setTempNoteText}
                         />
                     </Split>
                     :
